aab1d5d71f28ab612167b411bdba622bf9a95d8e
Bumpeet <pavanchetan81@gmail.com>
2023-08-29 11:34:23 +0530

    adding the new files which convert the physioNet data into hdf5 format

--- ConvNets/train.py
+++ ConvNets/train.py
@@ -7,16 +7,24 @@ from torch.utils.data import DataLoader
 # from torch.nn import functional as f
 # from torch import nn
 from torchvision.models import resnet50, ResNet50_Weights
+from tqdm import tqdm
+import math
+import numpy as np
 
 def prepare_dataset():
-    _transforms = transforms.Compose([transforms.ToTensor()])
-    train_dataset = DataLoader(ImageFolder(r"C:\Users\ATI-G2\Documents\python\ECG\data\dataRoot\train",transform=_transforms), batch_size=16)
+    _transforms = transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+    train_dataset = DataLoader(ImageFolder(r"C:\Users\ATI-G2\Documents\python\ECG\data\dataRoot\train",transform=_transforms), batch_size=8)
     val_dataset = DataLoader(ImageFolder(r"C:\Users\ATI-G2\Documents\python\ECG\data\dataRoot\val", transform=_transforms), batch_size=4)
 
     return train_dataset, val_dataset
 
 def model_definition():
     model = resnet50(ResNet50_Weights)
+
+    n_classes = 7
+    model.fc = nn.Linear(model.fc.in_features, n_classes)
     
     return model.train()
 
@@ -27,18 +35,39 @@ def main():
 
     model = model_definition().to(device)
 
-    optim = Adam(model.get_parameter())
+    optim = Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
 
     n_epochs = 2
 
+    # iter = 0
+    val_loss = []
+    train_loss = []
+
     for i in range(n_epochs):
-        for images, labels in train_dataset:
+        for images, labels in tqdm(train_dataset, f'epoch-{i+1}/{n_epochs}'):
             images, labels = images.to(device), labels.to(device)
-            probs = model(images)
             optim.zero_grad()
+            probs = model(images)
             loss = criterion(probs, labels)
-            loss.backwards()
+            loss.backward()
+            optim.step()
+            train_loss.append(loss.item())
+
+        print(f"training loss for the epoch-{i+1}/{n_epochs}: {sum(train_loss)/len(train_loss)}")
+        train_loss.clear()
+    
+        with torch.no_grad():
+
+            for images, labels in tqdm(val_dataset, "validating"):
+                images, labels = images.to(device), labels.to(device)
+                probs = model(images)
+                loss = criterion(probs, labels)
+                val_loss.append(loss.item())
+
+            print(f"val loss for the epoch-{i+1}/{n_epochs}: {sum(val_loss)/len(val_loss)}")
+            val_loss.clear()
+
 
 if __name__=="__main__":
     main()
---
+++ automatic-ecg-diagnosis/converting_to_hdf5.py
@@ -0,0 +1,128 @@
+import h5py
+import wfdb
+import numpy as np
+import glob
+import os
+from argparse import ArgumentParser
+import pandas as pd
+import re
+import csv
+
+def convert():
+    root_dir = args.ds
+    glob_path = root_dir + r"\\*\\*\\RECORDS" 
+
+    temp_arr = np.zeros((45000,3000,12))
+    k=0
+    # with open("records.txt","w") as f:
+    for i, pth in enumerate(glob.glob(glob_path)):
+        with open(f"{pth}", "r") as g:
+            records = g.readlines()
+            for record in records:
+                try:
+                    temp = pth
+                    fil = record.replace("\n",'')
+                    # print("hello",fil)
+                    pth = pth.replace("RECORDS",str(fil))
+                    signals, _ =  wfdb.rdsamp(pth, sampto=3000)
+                    # print(signals.shape)
+                    temp_arr[k,:,:] = signals
+                    # print(temp_arr[0])
+
+                except Exception as e:
+                    print(f'Exception cause in the record, {temp}, {record}, {k}')
+                    print(e)
+                    
+                print(k)
+                k += 1
+                pth = temp
+            
+                
+
+            
+
+    with h5py.File(f'12-lead.hdf5',"w") as f:
+        f.create_dataset("tracings",data=temp_arr)
+
+
+    with open("records.txt", "r") as f:
+        file = f.readline
+
+def convert_labels():
+    root_dir = args.ds
+    glob_path = root_dir + r"\\*\\*\\RECORDS" 
+
+    csv_pth = r"C:\Users\ATI-G2\Documents\python\ECG\data\code-15\12-lead\ConditionNames_SNOMED-CT.csv"
+    df = pd.read_csv(csv_pth)
+    label_idx = {}
+
+    all_labels = df["Snomed_CT"].values
+    for i, label in enumerate(all_labels):
+        label_idx[label] = i
+        # idx_label[i] = label
+
+    pattern = r"\d{3,}"
+
+    # print("labelsl",len(all_labels))
+    temp_arr = np.zeros((45000,len(all_labels)), dtype=np.int16)
+    k = 0
+    
+    for i, pth in enumerate(glob.glob(glob_path)):
+        with open(f"{pth}", "r") as g:
+            
+            records = g.readlines()
+            for record in records:
+                fil = record.replace("\n",'')
+                temp = pth
+                pth = pth.replace("RECORDS",str(fil))
+                try: 
+                    _, fields =  wfdb.rdsamp(pth, sampto=3000)
+                    string = fields["comments"][2]
+                    # print("string--", string)
+                    matches = re.findall(pattern, string)
+                    # print("These are the matches \n",matches)
+                    one_hot_vector = np.zeros((len(all_labels)),dtype=np.int16)
+                    for j,match in enumerate(matches):
+                        try:
+                            one_hot_vector[label_idx[int(match.rstrip())]] = 1
+                            # print(f"{match}-{i}")
+
+                        except Exception as e:
+                            print("exception",e)
+                            pass
+                    # print(i)
+                    # print(one_hot_vector)
+                    temp_arr[k,:]=one_hot_vector
+
+                except Exception as e:
+                    print(f'Exception cause in the record, {temp}, {record}, {k}')
+
+                pth = temp
+                print(k)
+                k +=1
+                
+
+    print("temp_arr",temp_arr.shape)
+
+    # with h5py.File("12-lead_labels.hdf5", "w") as f:
+    #     f.create_dataset("labels",data=temp_arr)
+
+    with open("12-lead_labels.csv","w",newline="\n") as f:
+        writer = csv.writer(f)
+        writer.writerows(temp_arr)
+
+
+    print("done with the creating labels hdf5")
+
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument("--ds", default=r"C:\Users\ATI-G2\Documents\python\ECG\data\code-15\12-lead\WFDBRecords")
+    return parser.parse_args()
+    
+
+if __name__ == "__main__":
+    args = get_args()
+    convert()
+    # convert_labels()
